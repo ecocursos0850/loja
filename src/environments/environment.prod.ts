524aa4d24fc292ac53e1f032d97796b9b7e656f8
Lefundes <angelolefundes@yahoo.com.br>
2024-05-08 09:08:43 -0300

    Correção do token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzEzODY0MTE1LCJleHAiOjE3MTQ1MDQ5MTV9.HlEAEEjGqPtlTQCr0U4wTGWlLG0KvJLHangJ5vfNPKk',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1MTY3NjkzLCJleHAiOjE3MTU4MDg0OTN9.26nrxU7W1nZpzlnxvW0D4iuF3HfoWl2gQJhY0POKnIY',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
