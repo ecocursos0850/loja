62baa43d5b4cbd65863350b2f7c0c0a8d5f36380
root <root@srv448021.hstgr.cloud>
2024-05-16 14:59:03 -0300

    Atualizado token

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODgyMjUwLCJleHAiOjE3MTY1MjMwNTB9.Ri4Kv9WO0GNE7zsNWLOq4l5oUt-smLFeocoZ8QnbtFw',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODgyMzQzLCJleHAiOjE3MTY1MjMxNDN9.OdkvUAZc0eoxs6aaa4Tl1GL1EvMdpOWSohonpRVs9oA',
   base_url: 'https://srv448021.hstgr.cloud:3000'
 };
