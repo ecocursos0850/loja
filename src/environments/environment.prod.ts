138e49306dc516fcc0b26c7f64fab438852707f7
Lefundes <angelolefundes@yahoo.com.br>
2024-05-16 09:08:19 -0300

    Novo token 16/04/2024

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1MTY3NjkzLCJleHAiOjE3MTU4MDg0OTN9.26nrxU7W1nZpzlnxvW0D4iuF3HfoWl2gQJhY0POKnIY',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODYxMDc5LCJleHAiOjE3MTY1MDE4Nzl9.WDFUXB9n3V3_WDCOzTbA2Bu5EDXF4jJ5khnxAehkdcQ',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzA1NTEyOTk5LCJleHAiOjE3MDYxNTM3OTl9.Da1hvOzzYAtDfccrXDnQIza2I5Pnrb3tmEEaJ-PYqfg',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODYxMDc5LCJleHAiOjE3MTY1MDE4Nzl9.WDFUXB9n3V3_WDCOzTbA2Bu5EDXF4jJ5khnxAehkdcQ',
   base_url: 'https://srv448021.hstgr.cloud:8080'
 };
