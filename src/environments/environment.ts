dcfcdaabc419d48800e7bd6c14fe64774a803e9a
Root <root@srv448021.hstgr.cloud>
2024-05-16 15:23:18 -0300

    Atualizado token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODgzNzIwLCJleHAiOjE3MTY1MjQ1MjB9.JcV7B-evZisqLpuQWRkG3lDkJiyBb-g-bEXkNWlBk5Y',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODgzNzk4LCJleHAiOjE3MTY1MjQ1OTh9.sR_plkO7_dP4Erg-z9-WNGT3bQrV2LWiVOcUs9Y1nqg',
   base_url: 'https://srv448021.hstgr.cloud:8080'
 };
