56fb1ccb1f6e88495c2b8af6031c0ba08ad6e03e
Lefundes <angelolefundes@yahoo.com.br>
2024-05-16 16:35:25 -0300

    Atualiza environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const Environment = {
   name: 'prod',
   token:
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODg1MTI4LCJleHAiOjE3MTY1MjU5Mjh9.Hnv6IbzGm2WzDCzwUSOogkZDmlfmXJNXSQ28ezS2gqA',
-  base_url: 'https://srv448021.hstgr.cloud:3000'
+  base_url: 'https://srv448021.hstgr.cloud:8443'
 };
