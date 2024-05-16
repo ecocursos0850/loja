94224af742a23fc84c6ab36a68a50b8bda7fc62a
Linux <user@ecocursos.com.br>
2024-05-16 19:17:40 -0300

    Correção da porta 8443

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const Environment = {
   name: 'dev',
   token:
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1ODk3MzczLCJleHAiOjE3MTY1MzgxNzN9.uzZRvS98imk8d5i5zGzxLBGegn01Lqo4_Q0Rh9E4wh4',
-  base_url: 'https://srv448021.hstgr.cloud:8080'
+  base_url: 'https://srv448021.hstgr.cloud:8443'
 };
