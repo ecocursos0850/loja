62f1dc94b55bfa199abbfc61bd270827ebc8d702
Lefundes <angelolefundes@yahoo.com.br>
2024-05-17 16:25:29 -0300

    Correção da porta

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const Environment = {
   name: 'dev',
   token:
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1OTczNzA1LCJleHAiOjE3MTY2MTQ1MDV9.XjqIdW02usL4MoMq7cPkAyR5W3Yo33OIEsFGnLJh1pM',
-  base_url: 'https://srv448021.hstgr.cloud:8080'
+  base_url: 'https://srv448021.hstgr.cloud:8443'
 };
