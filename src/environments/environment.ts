ce6f9d7d72441ff985f7966eb63386d780ee2e4d
Linux <user@ecocursos.com.br>
2024-05-17 16:01:49 -0300

    Renovação de token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1OTYxNTQxLCJleHAiOjE3MTY2MDIzNDF9.k4_bsElKFC7m-Jcdq-AjTMJk7uPJgCzaKgIHJVhxWig',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE1OTcyNTA5LCJleHAiOjE3MTY2MTMzMDl9.gXWlHJHxk2yQ56N6PuakdweMQoNOH-S5CyFqbd7NfJA',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
