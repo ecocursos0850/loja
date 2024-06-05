fcf6093b4d5dc68bfe0a378a630d86d8615dea47
Lefundes <angelolefundes@yahoo.com.br>
2024-06-05 12:18:20 -0300

    Porta PROD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const Environment = {
   name: 'dev',
   token:
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzE3NDA0NjAxLCJleHAiOjE3MTgwNDU0MDF9.Ip20lFmfcku0KirGzKr6CDHKJSGwL2i0errCe-w5Ym8',
-  base_url: 'https://srv448021.hstgr.cloud:3000'
+  base_url: 'https://srv448021.hstgr.cloud:8443'
 };
