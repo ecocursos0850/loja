40e3a285c1b4a6057b6ec0a928d95a17cc1a88e4
ecocursos0850 <168583932+ecocursos0850@users.noreply.github.com>
2024-07-19 13:45:03 -0300

    Update environment.ts PRD

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const Environment = {
   name: 'dev',
   token:
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzIxNDA3NDQ5LCJleHAiOjE3MjIwNDgyNDl9.CnlsXarSdIm1gnvR9VKifdrc_L8dHyw6aFmTjdQZyxk',
-  base_url: 'https://srv448021.hstgr.cloud:3000'
+  base_url: 'https://srv448021.hstgr.cloud:8443'
 };
