6993dfbfa19315b8f9e406eec15f046f49647311
ecocursos0850 <168583932+ecocursos0850@users.noreply.github.com>
2024-07-20 09:35:16 -0300

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const Environment = {
   name: 'dev',
   token:
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzIxNDY1OTQxLCJleHAiOjE3MjIxMDY3NDF9.UaNzltIESpxUndwMOKqodHn3NRer_hunUsbrtUzQMgc',
-  base_url: 'https://srv448021.hstgr.cloud:3000'
+  base_url: 'https://srv448021.hstgr.cloud:8443'
 };
