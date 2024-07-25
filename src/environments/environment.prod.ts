fda3510216e78399494c4516472a8a4f8ad7543b
Linux <user@ecocursos.com.br>
2024-07-25 05:14:02 -0300

    Atualização token 25072024051402

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzIxODk1MTgxLCJleHAiOjE3MjI1MzU5ODF9.rNCaWLJFQ6f9EhbGKulQ3EQQHvLN_R2-l2cUqk9trZw',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzIxODk1MjQxLCJleHAiOjE3MjI1MzYwNDF9.aVqj5tQWvkOPuChREyRjewePyZMswiks5DLPMZ4dTSw',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
