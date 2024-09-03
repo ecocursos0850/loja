afda53a50cc063672241c691d22113016953b30c
Linux <user@ecocursos.com.br>
2024-09-03 05:30:02 -0300

    Atualização token 03092024053002

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzI1MzUyMTQxLCJleHAiOjE3MjU5OTI5NDF9.bEI5xyyqQFfzm4rtlghdDgqneDf1QtveYCCVSqNJK_o',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzI1MzUyMjAyLCJleHAiOjE3MjU5OTMwMDJ9.e39IeYNygseH7TYWDxOcuxuaktWGz6JtgCvWiluPUKs',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
