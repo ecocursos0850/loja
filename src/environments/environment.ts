db1030a6a3130fe8213a50cec256806cdbe1c2cd
Linux <user@ecocursos.com.br>
2024-09-19 05:10:02 -0300

    Atualização token 19092024051002

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzI2NzMzMzQxLCJleHAiOjE3MjczNzQxNDF9.Eckaaz2Wz0eas_7gckJwA5IYxrVVbGvgtYNWGiCuyLs',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzI2NzMzNDAyLCJleHAiOjE3MjczNzQyMDJ9.pxp0h4sEtlq-JWHlY2wJpZKbLewF8dB0FuNdeUaeQ00',
   base_url: 'https://srv448021.hstgr.cloud:3000'
 };
