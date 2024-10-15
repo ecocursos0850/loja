cac50ee02ac26771b8db6a54981cc58636bd79e4
erickand16 <cerickandradebank@gmail.com>
2024-10-15 18:45:01 -0300

    fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const Environment = {
   name: 'dev',
   token:
     'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzI4OTgyNzQxLCJleHAiOjE3Mjk2MjM1NDF9.YXOAvIZA75TvR8YjorUBYEtiV3Jqtu_8nobMH9nL7sg',
-  base_url: 'https://srv448021.hstgr.cloud:3000'
+  base_url: 'https://srv448021.hstgr.cloud:8443'
 };
