dbd61a53cfc134746cb5186500eb5afffd271b2d
Linux <user@ecocursos.com.br>
2024-11-30 05:16:01 -0300

    Atualização token 30112024051601

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzMyOTU0NTAxLCJleHAiOjE3MzM1OTUzMDF9.llyjcDIVV4Lajc8_Tr1CuOfDpRpUMFQ5gOAdXN8ac_Y',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzMyOTU0NTYxLCJleHAiOjE3MzM1OTUzNjF9.7isumra7r7iC34Xg5_oTSTrCblVUGZgBDfQS-W-09l0',
   base_url: 'https://srv448021.hstgr.cloud:3000'
 };
