ed64ee579c3b992b7c20aff1a70f91bc6eb5fad6
Linux <user@ecocursos.com.br>
2025-02-23 05:45:02 -0300

    Atualização token 23022025054502

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzQwMzAwMjQyLCJleHAiOjE3NDA5NDEwNDJ9.7JO5kPuEpqpkZqAebDCDU7K011qwtiHKTau298fWTc8',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzQwMzAwMzAyLCJleHAiOjE3NDA5NDExMDJ9.7-ee9xJBlFRxpotSu2T0JzpbwAZQMKbrTDZdi6umBec',
   base_url: 'https://srv448021.hstgr.cloud:3000'
 };
