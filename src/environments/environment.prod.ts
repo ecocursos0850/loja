cdc1e330c063e98231d8d526ca54a7b2f5724650
Linux <user@ecocursos.com.br>
2025-05-12 05:35:01 -0300

    Atualização token 12052025053501

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzQ3MDM4ODQxLCJleHAiOjE3NDc2Nzk2NDF9.AIKrhVmHLRgpmnnKxt_ui5rbdvHUBM9A3-dsShv85FA',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzQ3MDM4OTAxLCJleHAiOjE3NDc2Nzk3MDF9.qPtsQ6KV0X8F0BP690j5_8_zlCZoHMHAisYWjkKKauE',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
