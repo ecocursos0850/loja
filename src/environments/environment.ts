9410aff1d13f3e203a28c2025997124c2494251a
Linux <user@ecocursos.com.br>
2025-08-01 05:29:01 -0300

    Atualização token 01082025052901

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU0MDM2ODgxLCJleHAiOjE3NTQ2Nzc2ODF9.fjd0bv1tct4u27o8goCpJiZsMsg3LdteqVrVYvfa98o',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU0MDM2OTQxLCJleHAiOjE3NTQ2Nzc3NDF9.3fwxkBtXVPDgpt-9xsLZNT972IYKM9RvLWR9YILGaMY',
   base_url: 'https://srv448021.hstgr.cloud:3000'
 };
