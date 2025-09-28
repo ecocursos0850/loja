fca1e3c244b7a008b66e110b03c7488618810aca
Linux <user@ecocursos.com.br>
2025-09-28 05:39:01 -0300

    Atualização token 28092025053901

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU5MDQ4NjgxLCJleHAiOjE3NTk2ODk0ODF9.NjwKVQ5ZSglazaz5U7ztO7M1bkmMmbn-pyAqMAMzFXE',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU5MDQ4NzQxLCJleHAiOjE3NTk2ODk1NDF9.__Kvxd1fgEjRTSRJJB-Yoi1ArAP-tEHUoeKL3tz3pAo',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
