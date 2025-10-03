96c2f0e6e7da9245ac1345154b5d8ca7d8a73f3e
Linux <user@ecocursos.com.br>
2025-10-03 05:34:01 -0300

    Atualização token 03102025053401

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU5NDgwMzgyLCJleHAiOjE3NjAxMjExODJ9.70MAw0ZKeJnFnw1PwDVQdZn8ObOlh19ibe-nNBNbYCo',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU5NDgwNDQxLCJleHAiOjE3NjAxMjEyNDF9.fgiTl15A0LbSXuy2APhDE4drSZAVGXvr7j75m77S0Mk',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU5NDgwMzgyLCJleHAiOjE3NjAxMjExODJ9.70MAw0ZKeJnFnw1PwDVQdZn8ObOlh19ibe-nNBNbYCo',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzU5NDgwNDQxLCJleHAiOjE3NjAxMjEyNDF9.fgiTl15A0LbSXuy2APhDE4drSZAVGXvr7j75m77S0Mk',
   base_url: 'https://srv448021.hstgr.cloud:3000'
 };
