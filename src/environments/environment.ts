feba2e9016d2d647aabcb14500bc11bb1481168e
Linux <user@ecocursos.com.br>
2025-11-11 05:57:01 -0300

    Atualização token 11112025055701

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: false,
   name: 'dev',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzYyODUxMzYxLCJleHAiOjE3NjM0OTIxNjF9.aH8eGatC70do0uVgS5mr-Qdm5fWys-Ccc4RgJ7AS1U0',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzYyODUxNDIxLCJleHAiOjE3NjM0OTIyMjF9.0Ryg2E4gq8p6sp-LR4p5WhD0SA7cABvKcsouiZv36VM',
   base_url: 'https://srv448021.hstgr.cloud:3000'
 };
