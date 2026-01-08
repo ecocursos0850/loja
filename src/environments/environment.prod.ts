c1232df10f0c6824eb1280eb471183b5851ccd32
Linux <user@ecocursos.com.br>
2026-01-08 05:30:02 -0300

    Atualização token 08012026053002

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const Environment = {
   production: true,
   name: 'prod',
   token:
-    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzY3ODYwOTQxLCJleHAiOjE3Njg1MDE3NDF9.P3p8r3qWPOnaWwVQhT1t-cIHY_8E1nCx4ndEC6WmGzs',
+    'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1c2VyQHVzZXIuY29tLmJyIiwiaWF0IjoxNzY3ODYxMDAyLCJleHAiOjE3Njg1MDE4MDJ9.UZDY8VkoHvxtNjItN3wyEJMqoSHK6p3_0xPghYmdq5k',
   base_url: 'https://srv448021.hstgr.cloud:8443'
 };
